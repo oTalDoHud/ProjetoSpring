d25d85a04819e1599ceff69137b4c3b805027827
oTalDoHud <hudson.lucas.vieira@gmail.com>
2021-11-29 11:25:40 -0300

    Intanciando automaticamente o banco de dados

--- src/main/java/com/hudLuca/cursoSpring/ProjetoSpringApplication.java
+++ src/main/java/com/hudLuca/cursoSpring/ProjetoSpringApplication.java
@@ -1,13 +1,32 @@
 package com.hudLuca.cursospring;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class ProjetoSpringApplication {
+import com.hudLuca.cursospring.domain.Categoria;
+import com.hudLuca.cursospring.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class ProjetoSpringApplication implements CommandLineRunner{
+	
+	@Autowired
+	private CategoriaRepository repo;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetoSpringApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		repo.saveAll(Arrays.asList(cat1, cat2));
+		
 	}
 
 }
