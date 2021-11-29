ebc97e3cfd93afb99b3fd5924b80812a840b1027
oTalDoHud <hudson.lucas.vieira@gmail.com>
2021-11-28 23:42:40 -0300

    Projeto Criado

---
+++ src/main/java/com/hudLuca/cursoSpring/ProjetoSpringApplication.java
@@ -0,0 +1,13 @@
+package com.hudLuca.cursoSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoSpringApplication.class, args);
+	}
+
+}
